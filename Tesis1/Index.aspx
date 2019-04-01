ec7b19f398d34fbcdc7c406f8ea629ee30cab822
asmino1701 <38118993+asmino1701@users.noreply.github.com>
2019-04-01 12:20:24 -0500

    Timer
    Añadido timer para ejecutar función cada 6 segundos

--- Tesis1/Index.aspx
+++ Tesis1/Index.aspx
@@ -4,7 +4,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="/Content/web.css" />
@@ -12,20 +12,20 @@
 <body>
     <form id="form1" runat="server" onsubmit="return false">
         <div id="container">
-        <!-- Stream video via webcam -->
-        <div class="video-wrap">
-            <video class="videoElement" id="video" playsinline autoplay></video>
-        </div>
+            <!-- Stream video via webcam -->
+            <div class="video-wrap">
+                <video class="videoElement" id="video" playsinline autoplay></video>
+            </div>
 
-        <div class="btn-primary">            
-            <button id="snap" onclick="EnviarImagen()">Capture</button>
-        </div>
+            <div class="btn-primary">
+                <button id="snap" onclick="EnviarImagen()">Capture</button>
+            </div>
 
-        <!-- Webcam video snapshot -->
-        <canvas id="canvas" width="640" height="480"></canvas>
-    </div>
+            <!-- Webcam video snapshot -->
+            <canvas id="canvas" width="640" height="480"></canvas>
+        </div>
     </form>
-     <!--Script references. -->
+    <!--Script references. -->
     <!--Reference the jQuery library. -->
     <script src="Scripts/jquery-3.3.1.min.js"></script>
     <!--Reference the SignalR library. -->
@@ -34,5 +34,11 @@
     <script src="signalr/hubs"></script>
     <!--Add script to update the page and send messages.-->
     <script type="text/javascript" src="/Scripts/webcam.js"></script>
+    <script>
+        //Timer para capturar frames cada 6 segundos
+        window.setInterval(function () {            
+            EnviarImagen();
+        }, 6000);
+    </script>
 </body>
 </html>
--- Tesis1/Scripts/webcam.js
+++ Tesis1/Scripts/webcam.js
@@ -1,4 +1,11 @@
-﻿const video = document.getElementById('video');
+﻿//Variables globales
+var chat = $.connection.chatHub;
+var context = canvas.getContext('2d');
+var imagenwc;
+var json;
+var temporizador;
+
+const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const snap = document.getElementById("snap");
 const constraints = {
@@ -9,14 +16,11 @@ const constraints = {
     }
 };
 
-var chat = $.connection.chatHub;
-var context = canvas.getContext('2d');
-var imagenwc;
-// Access webcam
+// Acceder a la webcam
 async function init() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        handleSuccess(stream);
+        handleSuccess(stream);        
     } catch (e) {
         //errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
         console.log("Something went wrong!");
@@ -33,38 +37,13 @@ function handleSuccess(stream) {
 init();
 
 // Draw image
-//snap.addEventListener("click", function (e) {
-//    e.preventDefault();
-//    context.drawImage(video, 0, 0, 640, 480);
-//    var imagenwc = canvas.toDataURL();
-
-//    imagenwc = imagenwc.replace(/^data:image\/(png|jpg);base64,/, "")
-//    document.getElementById("hdImage").value = imagenwc;
-//    var json = '{ "imageData" : "' + imagenwc + '" }';
-
-//    // Sending the image data to Server
-//    $.ajax({
-//        type: 'POST',
-//        url: 'Index.aspx/ImagenCls',
-//        data: json,
-//        contentType: "application/json; charset=utf-8",
-//        dataType: "json",
-//        success: function (msg) {
-//            alert("Done, Picture Uploaded.");
-//        },
-//        error: function (jqXHR, textStatus, errorThrown) {
-//            console.log(errorThrown);
-//        }
-//    });
-
-//    //enviarImagen(imagen);
-//});
-
-function EnviarImagen() {
+/*snap.addEventListener("click", function (e) {
+    e.preventDefault();
     context.drawImage(video, 0, 0, 640, 480);
-    imagenwc = canvas.toDataURL();
+    var imagenwc = canvas.toDataURL();
 
-    imagenwc = imagenwc.replace(/^data:image\/(png|jpg);base64,/, "");
+    imagenwc = imagenwc.replace(/^data:image\/(png|jpg);base64,/, "")
+    document.getElementById("hdImage").value = imagenwc;
     var json = '{ "imageData" : "' + imagenwc + '" }';
 
     // Sending the image data to Server
@@ -81,16 +60,43 @@ function EnviarImagen() {
             console.log(errorThrown);
         }
     });
+
+    //enviarImagen(imagen);
+});*/
+
+//Funcion para capturar la imagen desde el streaming
+function CapturarFrame() {
+    context.drawImage(video, 0, 0, 640, 480);
+    imagenwc = canvas.toDataURL();
+
+    imagenwc = imagenwc.replace(/^data:image\/(png|jpg);base64,/, "");
+    json = '{ "imageData" : "' + imagenwc + '" }';
+    EnviarImagen();
 }
 
-var temporizador;
-function f1() {
-    temporizador = setTimeout("callitrept()", 5000);
-}
-
-//SIGNAL R
-function enviarImagen(img) {
-    $.connection.hub.start().done(function () {
-        chat.server.send($(imagen));
+//Funcion para enviar la imagen al servidor
+function EnviarImagen() {
+    
+    // Sending the image data to Server
+    $.ajax({
+        type: 'POST',
+        url: 'Index.aspx/ImagenCls',
+        data: json,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            console.log("Bien.");
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
     });
 }
+
+
+//SIGNAL R
+//function enviarImagen(img) {
+//    $.connection.hub.start().done(function () {
+//        chat.server.send($(imagen));
+//    });
+//}
